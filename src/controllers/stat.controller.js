a7875f6a7f46739a2afec0ddec945540f1793c0f
NTrungAn <nguyentrunganhutech@gmail.com>
2026-03-20 00:20:47 +0700

    feat: Hoàn thành API thống kê và báo cáo

--- src/controllers/stat.controller.js
+++ src/controllers/stat.controller.js
@@ -1,4 +1,4 @@
-const statService = require('../services/stat.service');
+const statService = require("../services/stat.service");
 
 const getProjectStats = async (req, res) => {
   try {
@@ -19,7 +19,35 @@ const getUserDashboardStats = async (req, res) => {
   }
 };
 
+const getMemberPerformanceReport = async (req, res) => {
+  try {
+    const { projectId } = req.params;
+    const stats = await statService.getMemberPerformanceReport(
+      projectId,
+      req.user._id,
+    );
+    return res.status(200).json(stats);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+const getProjectTimelineReport = async (req, res) => {
+  try {
+    const { projectId } = req.params;
+    const stats = await statService.getProjectTimelineReport(
+      projectId,
+      req.user._id,
+    );
+    return res.status(200).json(stats);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getProjectStats,
   getUserDashboardStats,
+  getMemberPerformanceReport,
+  getProjectTimelineReport,
 };
